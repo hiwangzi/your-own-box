4f90697fb204c1d3c4ddd2add86ec4d8ff151c54
Luke Wang <me@lukewang.dev>
2023-11-23 21:29:29 +0800

    feat: init project

---
+++ src/main/java/dev/lukewang/yourownbox/YourOwnBoxApplication.java
@@ -0,0 +1,13 @@
+package dev.lukewang.yourownbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourOwnBoxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YourOwnBoxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/lukewang/yourownbox/YourOwnBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lukewang.yourownbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourOwnBoxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
